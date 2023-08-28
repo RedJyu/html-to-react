634668d38bd1e64bbe99682e32b06c30a22b02d4
RedJyu <piotr.stezycki@protonmai.com>
2023-08-28 15:03:22 +0200

    more dynamic code to practice

--- src/data.js
+++ src/data.js
@@ -22,3 +22,24 @@ export const socialLinks = [
     icon: 'fab fa-squarespace',
   },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: '  Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'saving money',
+    text: '  Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'saving money',
+    text: '  Lorem ipsum dolor sit amet consectetur adipisicing elit',
+  },
+];
